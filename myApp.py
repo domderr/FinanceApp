e2b59d2ea28b4e3065e16b5182cdfb2e07eae0b4
domenico derrico <70628898+domderr@users.noreply.github.com>
2020-12-17 21:56:04 +0000

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -11,171 +11,112 @@ import dash_table.FormatTemplate as FormatTemplate
 import plotly.graph_objs as go
 import numpy as np
 
-#list=pd.read_csv('C:/Users/Utente/Python/csv_lists\US1000.csv',sep=";",encoding='latin-1')
-d = {'Ticker': ['AAPL', 'MSFT','GOOG','AMZN','IBM'],
+def refresh_data():
+    d = {'Ticker': ['AAPL', 'MSFT','GOOG','AMZN','IBM'],
      'Name': ['Apple', 'Microsoft','Google','Amazon','Ibm']}
-list= pd.DataFrame(data=d)
-list_symbols=[]
-for symbol in list.Ticker:
-    list_symbols.append(symbol)
+
+    list= pd.DataFrame(data=d)
+    list['Name'] = list['Name'].str.slice(0,30)
+    list_symbols=[]
+
+    for symbol in list.Ticker:
+        list_symbols.append(symbol)
+
+    start="2015-1-1"
+    end="2020-12-31"
+    mylist= list_symbols
+
+    df = yf.download(mylist, start=start,end=end,group_by='Ticker', interval='1D')
+    df = df.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+    df['counter'] = df.groupby(['Ticker']).cumcount()
+    df['NetCh']   = df.groupby('Ticker').Close.pct_change()
     
-start="2019-7-1"
-end="2020-12-31"
-mylist= list_symbols
+    dates=df.index
+    df['Date']= dates.strftime('%Y-%m-%d')
 
-df = yf.download(mylist, start=start,end=end,group_by='Ticker', interval='1D')
-df = df.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
-df['counter']=df.groupby(['Ticker']).cumcount()
-df['NetCh']=df.groupby('Ticker').Close.pct_change()
-df['Avg2']=df.groupby('Ticker')['Close'].transform(lambda x: x.rolling(2, center=False).mean())
+    last_date_available=max(df.Date)
+    table_df=df[df.Date==last_date_available]
+    table_df=list.merge(table_df)
+    
+    return(list,df,table_df,last_date_available) 
 
-
-#Add column date as string
-dates=df.index
-df['Date']= dates.strftime('%Y-%m-%d')
-
-today=max(df.Date)
-table_df=df[df.Date==today]
-table_df=list.merge(table_df)
-table_df
+list,df,table_df,last_date_available=refresh_data()
 
 app = dash.Dash(__name__)
 server = app.server
 
-# We need to construct a dictionary of dropdown values  for the symbols 
 Ticker_options = []
 for ticker in range(0,len(list)):
     Ticker_options.append({'label':list.Name.loc[ticker],'value':list.Ticker.loc[ticker]})
     
-
+    
 app.layout = html.Div([
     dcc.Tabs([
         
-        dcc.Tab(label='Scanner', children=[
-            dash_table.DataTable(
-        id='datatable',
-        columns=[
-            {"name": 'Ticker',
-             "id": 'Ticker',
-             "deletable": False,
-             "selectable": False,
-             
-            },
-            {"name": 'Name',
-             "id": 'Name',
-             "deletable": False,
-             "selectable": False,
-             
-            },
-            {"name": 'NetCh',
-             "id": 'NetCh',
-             "deletable": False,
-             "selectable": False,
-             'type': 'numeric',
-             'format': FormatTemplate.percentage(2)
-            },
-            
-            {"name": 'Open',
-             "id": 'Open',
-             "deletable": False,
-             "selectable": False,
-             'type': 'numeric',
-             'format': FormatTemplate.money(2)
-            },
-             {"name": 'High',
-             "id": 'High',
-             "deletable": False,
-             "selectable": False,
-              'type': 'numeric',
-              'format': FormatTemplate.money(2)
-            },
-             {"name": 'Low',
-             "id": 'Low',
-             "deletable": False,
-             "selectable": False,
-              'type': 'numeric',
-              'format': FormatTemplate.money(2)
-            },
-            {"name": 'Close',
-             "id": 'Close',
-             "deletable": False,
-             "selectable": False,
-             'type': 'numeric',
-             'format': FormatTemplate.money(2)
-            },
-             {"name": 'Volume',
-             "id": 'Volume',
-             "deletable": False,
-             "selectable": False
-            }
-        ],
-        data=table_df.to_dict('records'),
-        editable=False,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="single",
-        column_selectable="none",
-        row_selectable="none",
-        row_deletable=False,
-        #column_deletable=False,
-        #selected_columns=[],
-        #selected_rows=[0],
-        page_action="native",
-        page_current= 0,
-        page_size= 20,
-        style_as_list_view=True,
-        #style_header={'backgroundColor': 'rgb(30, 30, 30)'},
-        #style_cell={
-        #    'backgroundColor': 'rgb(50, 50, 50)',
-        #    'color': 'white'
-        #    },
-        hidden_columns=['Adj Close','counter'],
-       
-        style_header={ 'border': '1px solid black' },
-        style_cell={
-            'height': 'auto',
-            # all three widths are needed
-            'minWidth': '50px', 'width': '50px', 'maxWidth': '50px',
-            'whiteSpace': 'normal'
-            },
-        #
-        #css=[{'selector': 'table', 'rule': 'table-layout: fixed'}]
+        dcc.Tab(html.Div([
+              
+                html.H1('Trading Analysis Dashboard'),
+                html.H4('developed by Domenico DErrico - www.TradingAlgo.tech'),
+
+                html.Div([
+                    html.Button(
+                        id='Refresh',
+                        n_clicks=0,
+                        children='Refresh',
+                        style={'fontSize':12, 'marginLeft':'10px'}
+                        ),
+                    ], style={'display':'inline-block'}),
+    
+
+                    dash_table.DataTable(
+                        id='datatable',
+                            columns=[
+                                {"name": 'Ticker', "id": 'Ticker'},
+                                {"name": 'Name'  , "id": 'Name' },
+                                {"name": 'NetCh' , "id": 'NetCh','type': 'numeric','format': FormatTemplate.percentage(2)},
+                                {"name": 'Open'  , "id": 'Open', 'type': 'numeric','format': FormatTemplate.money(2)},
+                                {"name": 'High'  , "id": 'High', 'type': 'numeric','format': FormatTemplate.money(2)},
+                                {"name": 'Low'   , "id": 'Low',  'type': 'numeric','format': FormatTemplate.money(2)},
+                                {"name": 'Close' , "id": 'Close','type': 'numeric','format': FormatTemplate.money(2)},
+                                {"Name": 'Volume', "id": 'Volume',},
+                                ]),
+
+                    html.Div('last date available '+last_date_available),
+
+                html.H5('NOTE: This App is for demonstration purpose only. Information made available is not an offer or solicitation of any kind in any jurisdiction '),
+
+        ])),
         
-    )
-        ]),
         dcc.Tab(label='Chart', children=[
             
-        # dropdown menu
-        html.Div([
-          dcc.Dropdown(id='ticker-picker',
-                       options=Ticker_options,
-                       value=list.Ticker.iloc[0],
-                       style={
-                           'color':'Black',
-                           #'border':'2px black solid',
-                           'padding':10,
-                           'width':1000,
-                           'height':50}
-                       #direction = "vertical"
-                      )]
-    ),
+            html.Div([
+              dcc.Dropdown(id='ticker-picker',
+                           options=Ticker_options,
+                           value=list.Ticker.iloc[0],
+                           style={'color':'Black','padding':10,'width':1000,'height':50})
+            ]),
 
-    html.Div([
-         dcc.Graph(id='graph',
-                   style={'color':'black', 'border':'2px black solid',
-                                     'borderRadius':5,'padding':10, 'width':1800,'height':700}
-                  )]
-    )    
-                   
-            
+            html.Div([
+                 dcc.Graph(id='graph',
+                           style={'color':'black', 'border':'2px black solid',
+                                  'borderRadius':5,'padding':10, 'width':1800,'height':700}
+            )])    
         ]),
     ])
 ])
 
+@app.callback(
+    Output('datatable', 'data'),
+    [Input('Refresh', 'n_clicks')])
+def update_table(n_clicks):
+    list,df,table_df,today=refresh_data()
+    data=table_df.to_dict('records')
+    
+    return(data)
 
 @app.callback(Output('graph', 'figure'),
               [Input('ticker-picker', 'value')])
-def update_figure(selected_ticker):                         # la funzione deve seguire ilcomanda @app
+def update_figure(selected_ticker): 
     filtered_df = df[df['Ticker'] == selected_ticker]
     
     trace0=go.Bar(
